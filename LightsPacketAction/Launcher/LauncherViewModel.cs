00c531defba94a9cbdca5a714c018e5acada8c2d
TechyPRO <jjosephttg@gmail.com>
2019-08-10 18:30:13 -0400

    Added Properties to Launcher VIew Model

--- LightsPacketAction/Launcher/LauncherViewModel.cs
+++ LightsPacketAction/Launcher/LauncherViewModel.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Input;
 
 namespace LightsPacketAction
 {
     public class LauncherViewModel
     {
+        public string ServerAddress { get; set; }
+        public string ServerPort { get; set; }
+        public string OverlayImagePath { get; set; }
+
+        public ICommand BrowseOverlayImageCommand { get; private set; }
+        public ICommand LaunchDisplayCommand { get; private set; }
+
+        public LauncherViewModel()
+        {
+            BrowseOverlayImageCommand = new RelayCommand((p) => {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                openFileDialog.Multiselect = false;
+                openFileDialog.Title = "Select Image to Overlay";
+                openFileDialog.Filter = "*.jpg | *.jpeg";
+
+                if (openFileDialog.ShowDialog() == true)
+                    OverlayImagePath = openFileDialog.FileName;
+            });
+
+            LaunchDisplayCommand = new RelayCommand((p) => {
+                Console.WriteLine("Not Implemented");
+            });
+        }
     }
 }
---
+++ LightsPacketAction/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace LightsPacketAction
+{
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> _execute = null;
+        readonly Predicate<object> _canExecute = null;
+
+        public RelayCommand(Action<object> execute)
+        {
+            _execute = execute;
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
