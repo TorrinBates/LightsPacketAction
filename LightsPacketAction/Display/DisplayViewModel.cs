3d7cec43157b94466b4e5dcf2dedfa0aa1f56d6c
Torrin <torrin.bates@promessinc.com>
2019-08-21 14:47:57 -0400

    Add error handling in display mode

--- LightsPacketAction/Display/DisplayViewModel.cs
+++ LightsPacketAction/Display/DisplayViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Dynamic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace LightsPacketAction
@@ -18,26 +20,56 @@ namespace LightsPacketAction
             Buttons = buttons;
             SendMessage = new RelayCommand(async p =>
             {
-                await Task.Run(() =>
+                try
                 {
-                    try
+                    await Task.Run(() =>
                     {
                         TcpClient client = new TcpClient(server, port);
 
-                        Byte[] data = Encoding.ASCII.GetBytes((string)p);
+                        Thread.Sleep(5000);
+                        Byte[] data = Encoding.ASCII.GetBytes((string) p);
 
                         NetworkStream stream = client.GetStream();
                         stream.Write(data, 0, data.Length);
 
                         stream.Close();
                         client.Close();
-                    }
-                    catch (SocketException e)
+                    });
+                }
+                catch (IOException)
+                {
+                    if (bbb == null)
                     {
-
+                        CreateErrorDialog("Connection lost, message not sent.");
                     }
-                });
+                }
+                catch (SocketException)
+                {
+                    if (bbb == null)
+                    {
+                        CreateErrorDialog("Connection lost, message not sent.");
+                    }
+                }
             });
+        }
+
+        public void CreateErrorDialog(string errorMessage)
+        {
+            CustomWindow window = null;
+            window = new CustomWindow(
+                new ErrorViewModel(errorMessage,
+                    new RelayCommand((param) =>
+                    {
+                        window.Close();
+                        bbb = null;
+                    })), "Error");
+            window.MinimizeVisibility = Visibility.Collapsed;
+            window.XVisibility = Visibility.Collapsed;
+            window.Owner = Application.Current.MainWindow;
+            window.Height = 100;
+            window.Width = 250;
+            bbb = window;
+            window.ShowDialog();
         }
 
         private bool _displayLines = false;
@@ -50,6 +82,8 @@ namespace LightsPacketAction
                 OnPropertyChanged("DisplayLines");
             }
         }
+
+        private CustomWindow bbb = null;
         public ICommand CloseDisplay { get; private set; }
         public ICommand SendMessage { get; private set; }
         public List<string> Buttons { get; private set; }
